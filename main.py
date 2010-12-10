f311bee10f79ede8cd21091f562fae6226f7094a
Joost Plattel <jsplattel@gmail.com>
2010-12-10 16:29:15 +0100

    added additional notes to main.py  On branch master

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext import db
 
+
+#database model... I'm aware that the two date's stored are the same but i use both!
 class Video(db.Model):
     title = db.StringProperty()
     pubdate = db.StringProperty()
@@ -36,20 +38,25 @@ class Video(db.Model):
     date_added = db.DateTimeProperty()
     watched = db.BooleanProperty()
     
-        
+
+#function to determine the RadBox id that is used in the URL    
 def RadBoxId(url):
     regex = re.compile("[0-9].*")
     r = regex.search(url)
     id = regex.findall(url)
     return id    
-    
+
+#fetch functions which pulls video's off the feed and plugs them in the datastore    
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        content = []
         
-        request_url = 'http://radbox.me/feed/video/home/c2d91689d2ebd9bf0d973a890d536e6f' #add apikey after the last slash
+        # Use the API-key RadBox provides to build a RSS feed
+        
+        apikey = 'c2d91689d2ebd9bf0d973a890d536e6f'
+        request_url = 'http://radbox.me/feed/video/home/' + apikey
         
         result = urlfetch.fetch(request_url)
+       
         if result.status_code == 200:
             result = result.content.replace('&laquo;', '')
             result = result.replace('&raquo;', '')
@@ -63,6 +70,7 @@ class MainHandler(webapp.RequestHandler):
             
             for node in dom.getElementsByTagName('item'):
                 
+                #highly inefficient code to clean up a string for using it as a date
                 item_id = node.getElementsByTagName('pubDate')[0].firstChild.data
                 item_id = item_id.replace(' EST', '')
                 item_id = item_id.replace('Mon, ', '')
@@ -76,6 +84,7 @@ class MainHandler(webapp.RequestHandler):
                 item_id = time.strptime(item_id, "%d %b %Y %H:%M:%S")
                 item_id = datetime.fromtimestamp(mktime(item_id))
                 
+                #compare the date to the last item know in the database, if it's more then add the video to the datastore
                 if item_id > last_id:
                     try: #try if the media:content is available to save
                         v = Video()
@@ -95,6 +104,7 @@ class MainHandler(webapp.RequestHandler):
                         v.url_radbox = node.getElementsByTagName('link')[0].firstChild.data
                         v.watched = False
                         v.put()
+                #else stop doing anything and just return to the homepage
                 else:
                     break
                     
@@ -102,7 +112,9 @@ class MainHandler(webapp.RequestHandler):
         
         self.redirect('/')
    
-   
+
+
+#video watchlist... Orderd by publication date, which is a string... I should modify this..   
 class VideoList(webapp.RequestHandler):
     def get(self, offset):    
         offset = int(offset)
@@ -121,28 +133,37 @@ class VideoList(webapp.RequestHandler):
         titles = []
         videos = db.GqlQuery("SELECT * FROM Video WHERE watched = False ORDER BY pubdate DESC LIMIT 100")
         for video in videos:
+            #if the video does not contain a youtube url, then open in an iFrame
             if video.url == '':
                 url = video.url_radbox
                 html = '<iframe class="frame" src="' + url + '" width="1024" height="728"><p>Your browser does not support iframes.</p></iframe>'
                 content.append(html)
                 video.title = '<a class="remove" href="/mark-as-watched/' + RadBoxId(video.url_radbox)[0] + '">&#215;</a> ' + video.title
                 titles.append(video.title)
+            #if it does contain a youtube video, embed at full width and height.
             else:                 
                 url = video.url
                 html = '<object width="1024" height="728"><param name="movie" value="' + url + '"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="' + url + '" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="1024" height="728"></embed></object>'
                 content.append(html)   
                 video.title = '<a class="remove" href="/mark-as-watched/' + RadBoxId(video.url_radbox)[0] + '">&#215;</a> ' + video.title
                 titles.append(video.title)
+        
+        #loop trough the video's
         try:
             content = content.pop(offset)
+        #if there are no more video's left:
         except:
             content = '<div class="end">You\'ve seen all footage!</div>'
-           
+        
+        #loop trough the video's to get titles        
         try:
             title = titles.pop(offset)
+        #if there are no more video's left:
         except:
             title = 'The end!'
-            
+         
+
+         
         template_values = {
             'content': content,
             'previous': previous,
@@ -155,12 +176,12 @@ class VideoList(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
 
         
-         
+#function to mark video's as watched... then redirect home.         
 class MarkAsWatched(webapp.RequestHandler):
     def get(self ,url_radbox): 
         
         url_radbox = 'http://radbox.me/watch/video/' + str(url_radbox)
-        videos = db.GqlQuery("SELECT * FROM Video WHERE url_radbox =:1 ORDER BY pubdate DESC LIMIT 2", url_radbox)
+        videos = db.GqlQuery("SELECT * FROM Video WHERE url_radbox =:1 ORDER BY pubdate DESC LIMIT 1", url_radbox)
         for video in videos:
             video.watched = True
             title = video.title
@@ -169,11 +190,13 @@ class MarkAsWatched(webapp.RequestHandler):
         
         self.redirect('/')
         
-        
+#if video is not specified redirect to the first one.        
 class VideoRedirect(webapp.RequestHandler):
     def get(self): 
         self.redirect('/video/0/')
-        
+
+
+#basic list of all unseen video's...         
 class Archives(webapp.RequestHandler):
     def get(self): 
         
@@ -202,7 +225,7 @@ class Archives(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
         
         
-        
+#touch this and the application is most likely to be useless on appengine unless you know what you are doing         
 def main():
     application = webapp.WSGIApplication([('/radbox/fetch/', MainHandler),
                                           ('/video/(.*)/', VideoList),
